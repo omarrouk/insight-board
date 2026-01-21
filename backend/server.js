f7e7867fa644645b7eb2fc18eb5fd5814db9a325
Omar Rouk <104861688+omarrouk@users.noreply.github.com>
2026-01-21 12:26:34 +0200

    Switch to ES6 import for dotenv configuration

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+import dotenv from 'dotenv';
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
